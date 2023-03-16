6b39696cedf71cc19c6f333f1ad015eadde49079
JibAb96 <j.abshir96@gmail.com>
2023-03-16 14:04:28 +0000

    Add code for computer random choice

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,32 @@
+let userTally = 0;
+let computerTally = 0;
+let scoreBoard_div = document.querySelector("score");
+let rockButton = document.getElementById("rock");
+let paperButton = document.getElementById("paper");
+let scissorsButton = document.getElementById("scissors");
+let userScoreShown = document.getElementById("user-score");
+let computerScoreShown = document.getElementById("comp-score");
+let result_p = document.querySelector(".result > p");
+
 function event() {
-    let rockButton = document.getElementById("rock");
     rockButton.addEventListener("click", function () {
         runGame("rock")
     });
-    let paperButton = document.getElementById("paper");
+
     paperButton.addEventListener("click", function () {
         runGame("paper")
     });
-    let scissorButton = document.getElementById("scissor");
+
     scissorButton.addEventListener("click", function () {
         runGame("scissor")
     })
 
 }
+
+function computerPick() {
+    picks = ["rock", "paper", "scissors"]
+    random = Math.floor(Math.random() * 3);
+    return picks[random]
+}
+
+function runGame
