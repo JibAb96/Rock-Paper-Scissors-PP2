ffe69d1082ef6e061521f2bdfa97b2cea2ce495e
JibAb96 <j.abshir96@gmail.com>
2023-03-17 11:58:35 +0000

    Add comments to javascript codethat are present so far

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// Declaring variables to be used in the functions comming up
 let userTally = 0;
 let computerTally = 0;
 let scoreDiv = document.querySelector("score");
@@ -8,30 +9,33 @@ let userScoreShown = document.getElementById("user-score");
 let computerScoreShown = document.getElementById("comp-score");
 let resultScript = document.querySelector(".result > p");
 let gameZone = document.querySelector("game")
-
+// Event listeners for the buttons on the game to trigger the game functions 
 function click() {
+    // Event listener for when the rock button is pressed 
     rockButton.addEventListener("click", function () {
         runGame("Rock")
     });
-
+    // Event listener for when the paper button is pressed
     paperButton.addEventListener("click", function () {
         runGame("Paper")
     });
-
+    // Event listener for when the scissors button is pressed
     scissorsButton.addEventListener("click", function () {
         runGame("Scissors")
     })
 
 }
-
+// A function for generating random choices for the computer
 function theComputerPick() {
     picks = ["Rock", "Paper", "Scissors"]
+    // Generates random numbers between 1 and 3
     random = Math.floor(Math.random() * 3);
     return picks[random]
 }
-
+// A function to determine the winner of the game and whether it's a draw 
 function runGame(userPick) {
     let computerChoice = theComputerPick();
+    // A switch statement that goes through each scenario of choices and the function to run   
     switch (userPick + computerChoice) {
         case "RockScissors":
         case "PaperRock":
@@ -51,22 +55,27 @@ function runGame(userPick) {
     }
     theWinner();
 }
-
+// A function that determines what happens when the user loses the game
 function win(userPick, computerPick) {
+    // Adds to the user tally when user wins the game  
     userTally++;
     userScoreShown.innerHTML = " " + userTally;
     computerScoreShown.innerHTML = " " + computerTally;
+    // Adds comment to page to show how the game played out     
     resultScript.innerHTML = `${userPick} wins over ${computerPick}. You're the winner! :D`
 }
-
+// A function that determines what happens when the user wins the game
 function lose(userPick, computerPick) {
+    // Adds to the computer tally when computer wins the game    
     computerTally++;
     computerScoreShown.innerHTML = " " + computerTally;
     userScoreShown.innerHTML = " " + userTally;
+    // Adds comment to page to show how the game played out     
     resultScript.innerHTML = `${userPick} loses to ${computerPick}. You've lost :(`
 }
-
+/* A function that determines what happens when the user draws the game */
 function draw(userPick, computerPick) {
+    // Adds comment to page to show how the game played out 
     resultScript.innerHTML = `${userPick} draws against ${computerPick}`
 }
 
